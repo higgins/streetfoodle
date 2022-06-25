e1057200807d22f7f9b632cf5af40874fa963777
Justin Higgins <justinhiggy@gmail.com>
2022-06-25 13:27:58 -0700

    fix state management, persistence to localstorage

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import Modal from 'react-modal';
 import Autosuggest from 'react-autosuggest';
 import './App.css';
-import data from './data.json'
+import FOOD_DATA from './data.json'
 
-const LOCAL_STORAGE_KEY = 'gameStats';
+const LOCAL_STORAGE_KEY = 'streetfoodle';
 
 const customStyles = {
   content: {
@@ -22,20 +22,22 @@ const customStyles = {
 };
 Modal.setAppElement('#root');
 
-function daysSinceEpoch() {
-  const current_date = new Date();
+function daysSinceEpoch(d) {
+  const current_date = d ? new Date(d) : new Date();
   const epocDate = new Date(new Date().getTime() / 1000);
   const res = Math.abs(current_date - epocDate) / 1000;
   return Math.floor(res / 86400);
 }
+
+const LAUNCH_DATE = daysSinceEpoch('06/25/2022');
 
 // Teach Autosuggest how to calculate suggestions for any given input value.
 const getSuggestions = value => {
   const inputValue = value.trim().toLowerCase();
   const inputLength = inputValue.length;
 
-  return inputLength === 0 ? [] : data.filter(food =>
-    food.name.toLowerCase().slice(0, inputLength) === inputValue
+  return inputLength === 0 ? [] : FOOD_DATA.filter(f =>
+    f.name.toLowerCase().slice(0, inputLength) === inputValue
   ).slice(0,8);
 };
 
@@ -107,131 +109,134 @@ class SuggestInput extends React.Component {
     );
   }
 }
+
+//{
+//  currentGame: 0,
+//  games: {
+//    0: {
+//      guesses: ['Hot Dog', 'Hamburger'],
+//      answer: 'blah',
+//    }
+//  }
+//}
+const DEFAULT_STATE = {
+  currentGame: null,
+  games: {}
+}
 function App() {
+  const [state, setState] = useState(DEFAULT_STATE);
   const [currentGuess, setCurrentGuess] = useState('');
-  const [numGuesses, setNumGuesses] = useState(3);
-  const [showRegion, setShowRegion] = useState(false);
-  const [showDescription, setShowDescription] = useState(false);
-  const [previousGuesses, setPreviousGuesses] = useState([]);
-  const [isLoser, setLoser] = useState(false);
-  const [isWinner, setWinner] = useState(false);
-  const [gameStats, setGameStats] = useState(null);
-  const [thisGameState, setThisGameState] = useState(null);
   const [statsIsOpen, setStatsModal] = useState(false);
   const [infoIsOpen, setInfoModal] = useState(false);
 
-  const today = daysSinceEpoch();
-  const foodIndex = today % data.length;
-  const food = data[foodIndex];
+  const daysSinceLaunch = daysSinceEpoch() - LAUNCH_DATE;
+  const currentGameIndex = daysSinceLaunch % FOOD_DATA.length;
+  const todaysFood = FOOD_DATA[currentGameIndex];
+  const openModal = (modalType) => () => modalType == 'stats' ? setStatsModal(true) : setInfoModal(true);
+  const closeModal = (modalType) => () => modalType == 'stats' ? setStatsModal(false) : setInfoModal(false);
 
   useEffect(() => {
-    if (!gameStats) {
-      const stats = localStorage[LOCAL_STORAGE_KEY] ? JSON.parse(localStorage[LOCAL_STORAGE_KEY]) : {};
-      if (stats.played) {
-        setGameStats(stats);
-        if (today === stats.lastPlayedDay) {
-          setNumGuesses(numGuesses - stats.lastPlayedGuesses.length);
-          setPreviousGuesses(stats.lastPlayedGuesses);
+    if (!state.currentGame) {
+      const cachedState = localStorage[LOCAL_STORAGE_KEY] ? JSON.parse(localStorage[LOCAL_STORAGE_KEY]) : null;
+      if (cachedState) {
+        const hydratedGames = cachedState.games;
+        if (!hydratedGames[currentGameIndex]) {
+          hydratedGames[currentGameIndex] = {
+            guesses: [],
+            answer: todaysFood.name,
+          };
         }
+        setState({
+          currentGame: currentGameIndex,
+          games: hydratedGames,
+        });
       } else {
-        setGameStats({ played: 0, won: 0, currentStreak: 0, maxStreak: 0, dist: [0,0,0], lastPlayedDay: null, lastPlayedGuesses: [] });
+        setState({
+          currentGame: currentGameIndex,
+          games: {
+            [currentGameIndex]: {
+              guesses: [],
+              answer: todaysFood.name,
+            }
+          }
+        });
       }
     }
-  }, [gameStats])
-
-  function openStatsModal() {
-    setStatsModal(true);
+  }, [state]);
+  if (!state.currentGame) {
+    return null;
   }
+  const currentGame = state.games[state.currentGame];
+  const previousGuesses = currentGame.guesses;
+  const guessesRemaining = 3 - previousGuesses.length;
+  const isWinner = currentGame.guesses[currentGame.guesses.length - 1] === todaysFood.name;
+  const isLoser = guessesRemaining <= 0 ? currentGame.guesses[currentGame.guesses.length - 1] !== todaysFood.name : false;
+  const regionClasses = guessesRemaining <= 2 ? "region self-start" : "hidden";
+  const descriptionClasses = guessesRemaining <= 1 ? "max-w-xs self-start leading-5" : "hidden";
 
-  function closeStatsModal() {
-    setStatsModal(false);
-  }
+  let guess1Classes = guessesRemaining < 3 ?  "rounded bg-red-600 p-2 m-1" : "rounded bg-slate-800 p-2 m-1";
+  let guess2Classes = guessesRemaining < 2 ?  "rounded bg-red-600 p-2 m-1" : "rounded bg-slate-800 p-2 m-1";
+  let guess3Classes = guessesRemaining < 1 ?  "rounded bg-red-600 p-2 m-1" : "rounded bg-slate-800 p-2 m-1";
 
-  function openInfoModal() {
-    setInfoModal(true);
-  }
-
-  function closeInfoModal() {
-    setInfoModal(false);
-  }
-
-  const regionClasses = showRegion ? "region self-start" : "hidden";
-  const descriptionClasses = showDescription ? "max-w-xs self-start leading-5" : "hidden";
-  const guess1Classes = numGuesses === 3 ? "rounded bg-slate-800 p-2 m-1" : "rounded bg-red-600 p-2 m-1";
-  const guess2Classes = numGuesses === 2 ? "rounded bg-slate-800 p-2 m-1" : ( numGuesses === 3 ? "rounded bg-slate-600 p-2 m-1" : "rounded bg-red-600 p-2 m-1" );
-  const guess3Classes = numGuesses === 1 ? "rounded bg-slate-800 p-2 m-1" : ( numGuesses >= 2 ? "rounded bg-slate-600 p-2 m-1" : "rounded bg-red-600 p-2 m-1" );
-
-  const updateStats = (wonGame, numGuesses) => {
-    const played = gameStats.played + 1;
-    const won = wonGame ? gameStats.won + 1 : gameStats.won;
-    const currentStreak = gameStats.lastPlayedDay ? (gameStats.lastPlayedDay + 1 === today ? gameStats.currentStreak + 1 : (wonGame ? 1 : 0)) : 1;
-    const maxStreak = gameStats.maxStreak > currentStreak ? gameStats.maxStreak : currentStreak;
-    const newDist = [...gameStats.dist];
-    if (wonGame) {
-      const guessIndex = Math.abs(numGuesses - 3);
-      newDist[guessIndex] = gameStats.dist[guessIndex] + 1;
+  if (isWinner) {
+    if (guessesRemaining === 2) {
+      guess1Classes = "rounded bg-lime-600 p-2 m-1";
+    } else if (guessesRemaining === 1) {
+      guess1Classes = "rounded bg-red-600 p-2 m-1";
+      guess2Classes = "rounded bg-lime-600 p-2 m-1";
+    } else {
+      guess1Classes = "rounded bg-red-600 p-2 m-1";
+      guess2Classes = "rounded bg-red-600 p-2 m-1";
+      guess3Classes = "rounded bg-lime-600 p-2 m-1";
     }
-    const updatedStats = {
-      played,
-      won,
-      currentStreak,
-      maxStreak,
-      dist: newDist,
-      lastPlayedDay: today
-    };
-    setGameStats(updatedStats);
-    localStorage[LOCAL_STORAGE_KEY] = JSON.stringify(updatedStats);
+  } else if (isLoser) {
+    guess1Classes = "rounded bg-red-600 p-2 m-1";
+    guess2Classes = "rounded bg-red-600 p-2 m-1";
+    guess3Classes = "rounded bg-red-600 p-2 m-1";
+  }
+  const numGamesPlayed = Object.values(state.games).filter((g) => (g.guesses.length === 3 || !!g.guesses.find(e => e === g.answer))).length;
+  const numGamesWon = Object.values(state.games).filter((g) => !!g.guesses.find(e => e === g.answer)).length;
+  const percentGamesWon = numGamesPlayed ? Math.round((numGamesWon / numGamesPlayed) * 100) : 0;
+  const numFirstGuess = Object.values(state.games).filter((g) => g.answer === g.guesses[0]).length
+  const numSecondGuess = Object.values(state.games).filter((g) => g.answer === g.guesses[1]).length
+  const numThirdGuess = Object.values(state.games).filter((g) => g.answer === g.guesses[2]).length
+
+  // NOTE: write to localStorage and component state
+  const saveState = (newState) => {
+    localStorage[LOCAL_STORAGE_KEY] = JSON.stringify(newState);
+    setState(newState);
   }
 
   const onGuess = () => {
     if (!currentGuess.length) {
       return
     }
-    // FIXME:
-    // updateBoard()
-      // update state
-      // update localStorage state
-    if (food.name === currentGuess) { // NOTE: correct guess
-      setShowRegion()
-      setShowDescription()
-      setWinner(true);
-      setThisGameState(numGuesses);
-      updateStats(true, numGuesses);
-      return
-    }
-    setPreviousGuesses([...previousGuesses, currentGuess])
-    setNumGuesses(numGuesses - 1);
+    const updatedGames = state.games;
+    updatedGames[state.currentGame].guesses.push(currentGuess);
+    const newState = { ...state, games: updatedGames };
+    saveState(newState);
     setCurrentGuess('');
-    if (numGuesses <= 1) {
-      setNumGuesses(0);
-      setLoser(true);
-      setThisGameState(0);
-      updateStats(false,4);
-      return
-    }
-    if (numGuesses === 2) {
-      setShowDescription(true)
-    }
-    if (numGuesses === 3) {
-      setShowRegion(true)
-    }
   }
+
   const onShareResults = () => {
     let emoji;
-    if (thisGameState === 3) {
+    const updatedGames = state.games;
+    const attempts = state.games[state.currentGame].guesses.length;
+
+    if (attempts === 1) {
       emoji = '🍏🍍🍍';
-    } else if (thisGameState === 2) {
+    } else if (attempts === 2) {
       emoji = '🍎🍏🍍';
-    } else if (thisGameState === 1) {
-      emoji = '🍎🍎🍏';
     } else {
-      emoji = '🍎🍎🍎'
+      emoji = isWinner ? '🍎🍎🍏' : '🍎🍎🍎';
     }
-    const clipBoard = `Street Foodle #FIXME\n${emoji}\n\nhttps://encapsulate.me/foodle`;
+    const clipBoard = `Street Foodle #${currentGameIndex + 1}\n${emoji}\n\nhttps://encapsulate.me/streetfoodle`;
     navigator.clipboard.writeText(clipBoard);
   }
-  const buttonText = numGuesses > 0 ? "SUBMIT" : "SHARE";
-  const previousGuessesDiv = previousGuesses.length ? previousGuesses.map((guess) => (<div className="self-start">{`❌ ${guess}`}</div>)) : null;
+  const buttonText = guessesRemaining > 0 ? "SUBMIT" : "SHARE";
+  const previousGuessesDiv = previousGuesses.length ? previousGuesses.map((guess) => {
+    return guess === todaysFood.name ? <div className="self-start">{`✅ ${guess}`}</div> : <div className="self-start">{`❌ ${guess}`}</div>;
+  }) : null;
   return (
     <div className="h-screen w-screen bg-slate-900 text-white font-medium text-lg">
       <div className="container mx-auto max-w-xs px-2.5">
@@ -240,23 +245,23 @@ function App() {
             <span id="logo" className="mx-1">StreetFoodle</span>
           </div>
           <div>
-            <span onClick={openStatsModal}><i className="fas fa-chart-simple mx-1"></i></span>
-            <span onClick={openInfoModal}><i className="far fa-question-circle mx-1"></i></span>
+            <span onClick={openModal('stats')}><i className="fas fa-chart-simple mx-1"></i></span>
+            <span onClick={openModal('info')}><i className="far fa-question-circle mx-1"></i></span>
           </div>
         </header>
         <div className="flex flex-col items-center">
-          <img src={food.imageURL} />
-          { isWinner && <div>You got it! That is "{food.name}"</div> }
-          { isLoser && <div>The answer was: "{food.name}"</div> }
+          <img src={todaysFood.imageURL} />
+          { isWinner && <div>You got it! That is "{todaysFood.name}"</div> }
+          { isLoser && <div>The answer was: "{todaysFood.name}"</div> }
           <div className="flex flex-row">
             <div className={guess1Classes}>1</div>
             <div className={guess2Classes}>2</div>
             <div className={guess3Classes}>3</div>
           </div>
-          <div className={regionClasses}><strong>Hint 1:</strong> {food.region}</div>
-          <div className={descriptionClasses}><strong>Hint 2:</strong> {food.description}</div>
+          <div className={regionClasses}><strong>Hint 1:</strong> {todaysFood.region}</div>
+          <div className={descriptionClasses}><strong>Hint 2:</strong> {todaysFood.description}</div>
           {
-            !isWinner && !isLoser && numGuesses > 0 &&
+            !isWinner && !isLoser && guessesRemaining > 0 &&
             <div className="flex flex-row justify-center items-center my-2">
               <SuggestInput currentGuess={currentGuess} onChange={setCurrentGuess} />
               <button className="ml-2 p-2 bg-slate-500 rounded-md hover:bg-slate-600 text-white" onClick={onGuess}>{buttonText}</button>
@@ -270,55 +275,55 @@ function App() {
           }
           { previousGuessesDiv }
         </div>
+        <div className="flex flex-col items-center">
+          <div className="absolute bottom-4">Made with <span>🌮</span> by <a className="text-sky-400 font-bold" target="_blank" href="https://twitter.com/justinprojects">Justin Higgins</a></div>
+        </div>
       </div>
       <Modal
         isOpen={statsIsOpen}
-        onRequestClose={closeStatsModal}
+        onRequestClose={closeModal('stats')}
         style={customStyles}
       >
         <div className="text-white bg-slate-700">
           <div className="flex flex-row justify-between">
             <div className="text-xl font-bold">STATS</div>
-            <button onClick={closeStatsModal}><i class="fa-solid fa-xmark"></i></button>
+            <button onClick={closeModal('stats')}><i class="fa-solid fa-xmark"></i></button>
           </div>
-          { gameStats &&
+          { state &&
             <>
               <div className="flex flex-row justify-between my-2">
                 <div className="flex flex-col justify-center items-center">
-                  <div className="font-medium text-xl">{gameStats.played}</div>
+                  <div className="font-medium text-xl">{numGamesPlayed}</div>
                   <div className="text-sm">Played</div>
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                  <div className="font-medium text-xl">{gameStats.won}</div>
+                  <div className="font-medium text-xl">{numGamesWon}</div>
                   <div className="text-sm">Won</div>
                 </div>
                 <div className="flex flex-col justify-center items-center">
-                  <div className="font-medium text-xl">{gameStats.played ? Math.round((gameStats.won / gameStats.played) * 100) : 0}</div>
+                  <div className="font-medium text-xl">{percentGamesWon}</div>
                   <div className="text-sm">Win %</div>
                 </div>
               </div>
-
-              <div className="font-medium leading-5 my-1"><strong>Current Streak: {gameStats.currentStreak}</strong></div>
-              <div className="font-medium leading-5 my-1"><strong>Max Streak: {gameStats.maxStreak}</strong></div>
               <div className="flex flex-row justify-between my-2">
                 <div className="text-xl font-bold">GUESS DISTRIBUTION</div>
               </div>
-              <div className="font-medium leading-5 my-1"><strong>First guess: {gameStats.dist[0]}</strong></div>
-              <div className="font-medium leading-5 my-1"><strong>Second guess: {gameStats.dist[1]}</strong></div>
-              <div className="font-medium leading-5 my-1"><strong>Third guess: {gameStats.dist[2]}</strong></div>
+              <div className="font-medium leading-5 my-1"><strong>First guess: {numFirstGuess}</strong></div>
+              <div className="font-medium leading-5 my-1"><strong>Second guess: {numSecondGuess}</strong></div>
+              <div className="font-medium leading-5 my-1"><strong>Third guess: {numThirdGuess}</strong></div>
             </>
           }
         </div>
       </Modal>
       <Modal
         isOpen={infoIsOpen}
-        onRequestClose={closeInfoModal}
+        onRequestClose={closeModal('info')}
         style={customStyles}
       >
         <div className="text-white bg-slate-700">
           <div className="flex flex-row justify-between">
             <div className="text-xl font-bold">ABOUT</div>
-            <button onClick={closeInfoModal}><i class="fa-solid fa-xmark"></i></button>
+            <button onClick={closeModal('info')}><i class="fa-solid fa-xmark"></i></button>
           </div>
           <div className="font-medium leading-5 my-2"><em>Street Foodle</em> is an idea by mi suegra (mother-in-law) Maria Eugenia Navarro.</div>
           <div className="font-medium leading-5 my-2"><strong>The rules:</strong> guess the name of a dish from around the world in less than 3 attempts. New dish every day.</div>
